d24c24f584593f59671507fcde1d654081ae76bd
margulan9 <sugirbaymargulan@gmail.com>
2020-07-11 13:46:39 +0600

    Commit from Tester branch

---
+++ Contacts/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  Contacts
+//
+//  Created by Sugirbay Margulan on 7/11/20.
+//  Copyright © 2020 Madiyar Kupzhassarov. All rights reserved.
+//
+
+import Foundation
+
+struct Test {
+    var a: String
+    var b: String
+}
