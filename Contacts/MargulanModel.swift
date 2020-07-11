5bb83700c8904d72729bfcfda8285ff3a786a8ce
margulan9 <sugirbaymargulan@gmail.com>
2020-07-11 13:24:48 +0600

    Commit from Margo

---
+++ Contacts/MargulanModel.swift
@@ -0,0 +1,14 @@
+//
+//  MargulanModel.swift
+//  Contacts
+//
+//  Created by Sugirbay Margulan on 7/11/20.
+//  Copyright © 2020 Madiyar Kupzhassarov. All rights reserved.
+//
+
+import Foundation
+
+struct Margo {
+    let name: String
+    var age: Int
+}
